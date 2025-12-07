d4656915d10ab925464e7a06c2fb03edc8415faf
dattakale21 <dattakale2008@gmail.com>
2025-12-08 04:03:10 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/SK-GLOBAL-EXIM/",
   plugins: [react()],
+  server: {
+    host: true,  // 👈 allow external devices
+    port: 5173   // optional: default port
+  },
 })
